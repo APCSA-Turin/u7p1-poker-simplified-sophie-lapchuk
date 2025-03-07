06c735885021a4b68cdb2512d40f5661202645dd
Sophie Lapchuk <sophiel40@nycstudents.net>
2025-03-07 03:52:29 +0000

    Base project commit

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -3,14 +3,77 @@ import java.util.ArrayList;
 
 
 public class Game{
+// Simulates the game based on the hand entered as a parameter
+// Compares the numerical rank of each hand. If they are equal, the highest card is compared
+// The player with the higher ranking card wins the game.
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
-        return "Error";
+        int p1Rank = Utility.getHandRanking(p1Hand);
+        int p2Rank = Utility.getHandRanking(p2Hand);
+
+        if (p1Rank > p2Rank) {
+            return "Player 1 wins!";
+        }
+        if (p1Rank < p2Rank) {
+            return "Player 2 wins!";
+        }
+        else {
+                // locates the highest card that is in the hand-- not the community
+                p1.sortHand();
+                p2.sortHand();
+                Card p1HighCard = p1.getHand().get(1);
+                Card p2HighCard = p2.getHand().get(1);
+                int p1HighRank = Utility.getRankValue(p1HighCard.getRank());
+                int p2HighRank = Utility.getRankValue(p2HighCard.getRank());
+
+                if (p1HighRank > p2HighRank) {
+                    return "Player 1 wins!";
+                }
+                if (p1HighRank < p2HighRank) {
+                    return "Player 2 wins!";
+                }
+                else {
+                    return "Tie!";
+                }
+        }
     }
 
-    public static void play(){ //simulate card playing
-    
+    public static void play() { //simulate card playing
+        // Will be finished for EC.
     }
-        
-        
 
+// Test code for debugging
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        
+        player1.addCard(new Card("A", "♠"));
+        player1.addCard(new Card("A", "♣"));
+        
+        player2.addCard(new Card("8", "♠"));
+        player2.addCard(new Card("8", "♣"));
+        
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(new Card("7", "♦"));
+        communityCards.add(new Card("7", "♥"));
+        communityCards.add(new Card("7", "♠"));
+        
+        String p1Result = player1.playHand(communityCards);
+        String p2Result = player2.playHand(communityCards);
+        
+        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
+
+        System.out.println("Player 1 hand: " +  player1.getAllCards());
+        System.out.println("Player 2 hand: " + player2.getAllCards());
+        System.out.println("Player 1 hand NO COMMUNITY CARDS: " +  player1.getHand());
+        System.out.println("Player 2 hand NO COMMUNITY CARDS: " + player2.getHand());
+        System.out.println("Value of player 1 cards: " + Utility.getRankValue(player1.getHand().get(1).getRank()));
+        System.out.println("Value of player 2 cards: " + Utility.getRankValue(player2.getHand().get(1).getRank()));
+        boolean bool = (Utility.getRankValue(player1.getHand().get(1).getRank())) > Utility.getRankValue(player2.getHand().get(1).getRank());
+        System.out.println("Is player 1 > player 2?: " + bool);
+        System.out.println("Community Cards: " + communityCards.toString());
+        System.out.println("Winner: " + winner);
+        System.out.println();
+        System.out.println(p1Result);
+        System.out.println(p2Result);
+    }
 }
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -10,6 +10,7 @@ public class Player{
     
     public Player(){
         hand = new ArrayList<>();
+        allCards = new ArrayList<>();
     }
 
     public ArrayList<Card> getHand(){return hand;}
@@ -20,6 +21,46 @@ public class Player{
     }
 
     public String playHand(ArrayList<Card> communityCards){     //if statements to return what kind of hand it is  
+        allCards.clear();
+        allCards.addAll(hand);
+        allCards.addAll(communityCards); //combine all the cards
+        sortAllCards();
+        //check and return proper string for the type of hand
+        if (isRoyalFlush()) {
+            return "Royal Flush";
+        } 
+        if (isStraightFlush()) {
+            return "Straight Flush";
+        }
+        if (isFourOfAKind()) {
+            return "Four of a Kind";
+        }
+        if (isFullHouse()) {
+            return "Full House";
+        }
+        if (isFlush()) {
+            return "Flush";
+        }
+        if (isStraight()) {
+            return "Straight";
+        }
+        if (isThreeOfAKind()) {
+            return "Three of a Kind";
+        }
+        if (isTwoPair()) {
+            return "Two Pair";
+        }
+        if (isPair()) {
+            return "A Pair";
+        }
+        // If no hand is found, search for the highest card in the player's hand
+        Card highCard = allCards.get(4);
+        for (Card card : hand) {
+            if (card.equals(highCard)) {
+                return "High Card";
+            }
+        }
+        // If the highest card is in the community, there is no hand
         return "Nothing";
     }
 
@@ -27,33 +68,208 @@ public class Player{
     //make methods from the worst hand to use while going up
     //if double and triple
 
-    public void sortAllCards(){} // return last element to get the max
-    // use sorted list to loop through it and check if the hand is a straight if the difference between each is 1
+// Implementation of a sorting algorithm to sort all cards based on ascending order of a rank
+    public void sortAllCards(){
+        for (int i = 0; i < allCards.size(); i++) {
+            int min = i;
+            for (int j = i + 1; j < allCards.size(); j++) {
+                if (Utility.getRankValue(allCards.get(j).getRank()) < Utility.getRankValue(allCards.get(min).getRank())) {
+                    Card temp = allCards.get(j);
+                    allCards.set(j, allCards.get(min));
+                    allCards.set(min, temp);
+                }
+            }
+        }
+    }
 
+// Organizing the two cards in a player's hand in ascending order
+    public void sortHand() {
+        int rankCard1 = Utility.getRankValue(hand.get(0).getRank());
+        int rankCard2 = Utility.getRankValue(hand.get(1).getRank());
+        if (rankCard1 > rankCard2) {
+            Card temp = hand.get(0);
+            hand.set(0, hand.get(1));
+            hand.set(1, temp);
+        }
+    }
+        // use sorted list to loop through it and check if the hand is a straight if the difference between each is 1
+        // return last element to get the max
+
+// Mariam helped me with this method
+// Finding the frequency of the rank occurring in the hand
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> freq = new ArrayList<>();
+        for (int i = 0; i < ranks.length; i++) {
+            freq.add(0); //add a 0 slot for every rank
+        }
+
+        for (Card card : allCards) {
+            int idx = Utility.getRankValue(card.getRank()) - 2; //move down 2 because rank values start at 2, not 0
+            freq.set(idx, freq.get(idx) + 1); //increment one to count occurances in allCards 
+        }
+        return freq;
     }
 
+// Finding the frequency of each suit occurring in a player's hand
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> freq = new ArrayList<>();
+        for (int j = 0; j < Utility.getSuits().length; j++) {
+            freq.add(0); //add 0 for every slot
+        }
+        int i = 0;
+        for (Card card : allCards) {
+            if (card.getSuit().equals(suits[i])) {
+                freq.set(i, freq.get(i) + 1); //if another suit is found, increment number of occurances
+            }
+        }
+        return freq; 
     }
-
    
     @Override
     public String toString(){
         return hand.toString();
     }
 
-    public static void main(String[] args) {
-        // testing addCard
-        Player player = new Player();
-        player.addCard(new Card(Utility.getSuits()[0], Utility.getRanks()[1]));
-        System.out.println(player.getHand());
-
-        //
+// Searches for 2 occurrances of one rank in a hand
+    public boolean isPair() {
+        ArrayList<Integer> freq = new ArrayList<>();
+        for (int i = 0; i < ranks.length; i++) {
+            freq.add(0);
+        }
+        freq = findRankingFrequency();
+        for (int num : freq) {
+            if (num == 2) {
+                return true;
+            }
+        }
+        return false;
     }
 
+// Searches for 2 occurances of two different pairs
+    public boolean isTwoPair() {
+        if (isPair()) {
+            int pairCount = 0;
+            // find number of pairs
+            for (int freq : findRankingFrequency()) {
+                if (freq == 2) {
+                    pairCount++;
+                }
+            }
+            // if it is 2, then it is a two pair
+            if (pairCount == 2) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+// Similar to the Pair, searches for 3 of a rank in a hand
+    public boolean isThreeOfAKind() {
+        for (int num : findRankingFrequency()) {
+            if (num == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+// Checks if the difference between each rank in the deck is 1. If it is, it means that the cards are incrementing in order.
+    public boolean isStraight() {
+        boolean bool = true;
+        for (int i = 0; i < allCards.size() - 1; i++) {
+            if (Utility.getRankValue(allCards.get(i+1).getRank()) - Utility.getRankValue(allCards.get(i).getRank()) != 1) {
+                    bool = false;
+            }
+        }
+        return bool;
+    }
 
+// Checks the hand to see if each card is of one suit. 
+    public boolean isFlush() {
+        for (int freq : findSuitFrequency()) {
+            if (freq == 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+// Checks the hand for one set of 3 cards of one rank, and a set of 2 cards of another rank
+    public boolean isFullHouse() {
+        boolean has3 = false;
+        boolean has2 = false;
+        for (int freq : findRankingFrequency()) {
+            if (freq == 3) {
+                has3 = true;
+            }
+            if (freq == 2) {
+                has2 = true;
+            }
+        } 
+        return has3 && has2;
+    }
+
+// Searches for 4 occurrances of one rank in the hand
+    public boolean isFourOfAKind() {
+        for (int freq : findRankingFrequency()) {
+            if (freq == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+// Checks if the hand is a straight AND a flush. If it is, then it has to be a straight flush
+    public boolean isStraightFlush() {
+        return isFlush() && isStraight();
+    }
+
+// Checks that the hand is a straight AND the smallest card is a 10. If it is, it must go 10, J, Q, etc., so it is a royal flush
+    public boolean isRoyalFlush() {
+        sortAllCards();
+        return isStraightFlush() && (Utility.getRankValue(allCards.get(0).getRank()) == 10);
+    }
+
+//{"♠","♥","♣", "♦"};
+// Test code for debugging
+    public static void main(String[] args) {
+        Player player = new Player();
+        Card a = new Card("A", "♠");
+        Card b = new Card("A", "♣");
+        Card c = new Card("7", "♦");
+        Card d = new Card("7", "♥");
+        Card e = new Card("7", "♠");
+        player.addCard(a);
+        player.addCard(b);
+
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(c);
+        communityCards.add(d);
+        communityCards.add(e);
+        System.out.println("Unsorted deck: " + player.getHand());
+        player.sortAllCards();
+        System.out.println("Sorted deck: " + player.getHand());
+        System.out.println("Sorted Full Deck: " + player.getAllCards());
+        player.playHand(communityCards);
+        System.out.println("Played hand: " + player.playHand(communityCards));
+        System.out.println("Full Deck: " + player.getAllCards());
+
+        System.out.println();
+        System.out.println("Frequency List: " + player.findRankingFrequency());
+        
+        
+    }
+    /*
+     * player1.addCard(new Card("A", "♠"));
+        player1.addCard(new Card("A", "♣"));
+        
+        player2.addCard(new Card("8", "♠"));
+        player2.addCard(new Card("8", "♣"));
+        
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(new Card("7", "♦"));
+        communityCards.add(new Card("7", "♥"));
+        communityCards.add(new Card("7", "♠"));
+     */
 }
+
